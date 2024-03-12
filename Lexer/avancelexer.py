6e58cb4d90f7d0a77ddca081aa22041ad23e38a3
IDxpro <84549452+IDxpro@users.noreply.github.com>
2024-03-12 00:24:20 -0600

    Update avancelexer.py

--- Lexer/avancelexer.py
+++ Lexer/avancelexer.py
@@ -135,32 +135,3 @@ token_list = tk.Text(frame, width=50, height=20)
 token_list.pack(side=tk.RIGHT)
 
 root.mainloop()
-avancelexer.py
-Mostrando avancelexer.py.
-Avances en la Construcción de tu Traductor
-MICHEL EMANUEL LOPEZ FRANCO
-•
-19 feb (Última modificación: 23 feb)
-80
-/100
-80 puntos de 100
-Fecha de entrega: 4 mar, 1:00
-Objetivo:
-
-Desarrollar un componente específico de un traductor (puede ser un compilador o un intérprete) para un lenguaje de programación simplificado. Este proyecto se realizará en varias fases, cada una centrada en una parte diferente del proceso de traducción.
-Fase Actual: Análisis Léxico y sintactico 
-
-Descripción:
-En esta fase, deberás implementar el analizador léxico para el lenguaje de programación asignado. El lenguaje tendrá una sintaxis y un conjunto de tokens definidos previamente por el instructor. El analizador léxico deberá ser capaz de leer el código fuente y convertirlo en una secuencia de tokens que serán utilizados en fases posteriores del proceso de traducción.
-Requerimientos:: Basado en la especificación del lenguaje proporcionada, define los tokens que formarán parte del lenguaje. Esto incluye palabras reservadas, identificadores, literales numéricos, operadores, etc.
-: Escribe tu propio analizador en el lenguaje de programación de tu elección. El analizador debe poder leer un archivo de entrada con código fuente y producir una lista de tokens.
-: Tu analizador léxico debe ser capaz de manejar y reportar errores léxicos de manera adecuada, como caracteres inválidos o formatos incorrectos de tokens.
-Entregables:: El código fuente de tu analizador léxico y sintactico, incluyendo cualquier estructura de datos utilizada para almacenar los tokens.
-: Un breve documento que explique tu diseño, las decisiones importantes que tomaste durante la implementación, y cómo se manejan los errores léxicos y sintacticos.
-: Archivos de entrada de muestra junto con la salida producida por tu analizador léxico y sintactico. Incluye casos que demuestren el manejo correcto de los tokens y también ejemplos que muestren cómo se manejan los errores léxicos.
-Evaluación:
-Tu tarea será evaluada en base a los siguientes criterios:
-: ¿Tu analizador léxico identifica correctamente todos los tokens definidos en la especificación del lenguaje?
-: ¿Tu analizador proporciona mensajes de error útiles y precisos para entradas inválidas?
-: ¿Tu código está bien organizado, comentado y sigue las buenas prácticas de programación? ¿La documentación proporciona una buena visión general de tu implementación y decisiones
-Comentarios de la clase
