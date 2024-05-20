238dd648bf8818b75ef7d5280aa2568180102f20
IDxpro <84549452+IDxpro@users.noreply.github.com>
2024-05-20 10:33:43 -0600

    Update semantic.py

--- TDL2P/semantic.py
+++ TDL2P/semantic.py
@@ -1,5 +1,6 @@
-
 import re
+import ast
+
 
 def analizador_semantico(codigo):
     # Expresión regular para detectar declaraciones de variables y asignaciones
@@ -18,15 +19,35 @@ def analizador_semantico(codigo):
     for var_destino, var1, operador, var2 in coincidencias_asignacion:
         print(f"Se encontró una asignación: {var_destino} = {var1} {operador} {var2}")
 
+
+def creador_ast(codigo):
+    # Parsear el código en un AST
+    tree = ast.parse(codigo)
+
+    # Función para imprimir el AST de manera estilizada
+    def pprint_ast(node, indent=0, is_leaf=False):
+        if isinstance(node, ast.AST):
+            print(' ' * indent + f"Nodo: {type(node).__name__}")
+            for child_node in ast.iter_child_nodes(node):
+                pprint_ast(child_node, indent + 4)
+        else:
+            print(' ' * indent + f"Hoja: {node}")
+
+    # Imprimir el AST estilizado
+    pprint_ast(tree)
+
+
 # Código de ejemplo
 codigo_c = """
-int main(){
-float a;
-int b;
-int c;
-c = a+b;
-c = suma(8,9);
-}
+"""
+
+"""
+def main():
+  a = 10
+  b = 20
+  c = a - b
+  return 0
 """
 
 analizador_semantico(codigo_c)
+creador_ast(codigo_c)
